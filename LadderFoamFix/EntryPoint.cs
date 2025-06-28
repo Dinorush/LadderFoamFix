99a8b47b3952fed1d057092433c2936afe342b51
Dinorush <onparnorthstar@gmail.com>
2025-06-28 02:45:05 -0400

    Add project files.

---
+++ LadderFoamFix/EntryPoint.cs
@@ -0,0 +1,18 @@
+﻿using BepInEx;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+
+namespace LadderFoamFix
+{
+    [BepInPlugin("Dinorush." + MODNAME, MODNAME, "1.0.0")]
+    internal sealed class EntryPoint : BasePlugin
+    {
+        public const string MODNAME = "LadderFoamFix";
+
+        public override void Load()
+        {
+            new Harmony(MODNAME).PatchAll();
+            Log.LogMessage("Loaded " + MODNAME);
+        }
+    }
+}
---
+++ LadderFoamFix/FoamHitboxPatch.cs
@@ -0,0 +1,29 @@
+﻿using Enemies;
+using HarmonyLib;
+
+namespace LadderFoamFix
+{
+    [HarmonyPatch(typeof(ES_StuckInGlue))]
+    internal static class FoamHitboxPatch
+    {
+        [HarmonyPatch(nameof(ES_StuckInGlue.CommonEnter))]
+        [HarmonyPostfix]
+        private static void DisableHitbox(ES_StuckInGlue __instance)
+        {
+            var ai = __instance.m_ai;
+            ai.m_behaviour.m_updatebehaviour = float.MaxValue;
+            ai.SetNavMeshAgent(false);
+        }
+
+        [HarmonyPatch(nameof(ES_StuckInGlue.CommonExit))]
+        [HarmonyPostfix]
+        private static void EnableHitbox(ES_StuckInGlue __instance)
+        {
+            var ai = __instance.m_ai;
+            ai.m_behaviour.m_updatebehaviour = 0f;
+            var state = __instance.m_locomotion.m_currentState.m_stateEnum;
+            if (state != ES_StateEnum.Dead && state != ES_StateEnum.Hibernate)
+                ai.SetNavMeshAgent(true);
+        }
+    }
+}
